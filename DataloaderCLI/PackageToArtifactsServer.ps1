746001e1b2de3a9510cecbba263271f6d1ec4036
Arnþór Ingi Hinriksson <arnthor.hinriksson@gmail.com>
2016-01-19 16:38:29 +0000

    added ps1 script to copy buildresults

--- DataloaderCLI/PackageToArtifactsServer.ps1
+++ DataloaderCLI/PackageToArtifactsServer.ps1
@@ -1,27 +1,11 @@
 #create folder called DataLoaderCLI_%VERSION%
-$NewFolderName = $args[0]
+$NewFolderName = 'DataLoaderCLI_'+$args[0]
 
 $FolderFUllPath = '\\sec-it-artifacts\Software\'+$NewFolderName
 
-#New-Item 
 New-Item $FolderFUllPath -ItemType Directory
 
 
-#Copy from: \DataloaderCLI\DataloaderCLI\bin\Release
-		#Aih.Dataloader.dll
-		#DataloaderCLI
-
 Copy-Item DataLoaderCLI\DataloaderCLI\bin\Release\Aih.Dataloader.dll $FolderFUllPath
 Copy-Item DataLoaderCLI\DataloaderCLI\bin\Release\DataloaderCLI.exe $FolderFUllPath
-# 
-
-#Copy from DataloaderCLI\DataloaderCLI
-		#conf.ini
-Copy-Item DataLoaderCLI\DataloaderCLI\conf.ini
-
-
-#Move folder to: \\sec-it-artifacts\Software
-
-
-
-#Delete folder created at the begining
+Copy-Item DataLoaderCLI\DataloaderCLI\conf.ini $FolderFUllPath
